ac3f2a6c7352bdedc3b5eb0aca43c7c91650c3c4
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 20:51:08 -0300

    Resolução exercício 08

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_08.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_08 {
+    public static void main(String[] args) {
+        //08.  Verificar número primo
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número:");
+        int numero = scanner.nextInt();
+        boolean ehPrimo = verificarPrimo(numero);
+        if (ehPrimo) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
+    public static boolean verificarPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
