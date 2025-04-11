3a4de026a4023bc3ea47173ab63b752429f04d00
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 19:36:31 -0300

    Resolução exercício 02

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_02.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_02 {
+    public static void main(String[] args) {
+        //02. Soma de 1 a N
+        int N = 5;
+        int soma = 0;
+        for (int i = 1; i <= N; i++) {
+            soma+= i;
+        }
+        System.out.println(soma);
+        }
+    }
