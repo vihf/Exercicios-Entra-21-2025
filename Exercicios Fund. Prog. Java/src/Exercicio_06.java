7254a3b5d9e1b06f669aafb72bed53683f360a14
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 20:09:04 -0300

    Resolução exercício 06

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_06.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_06 {
+    public static void main(String[] args) {
+        //06. Fatorial de um número
+        int fatorial = 5;
+        for (int i = 4; i > 0; i--) {
+            fatorial *= i;
+        }
+        System.out.println("Fatorial de 5: " + fatorial);
+        }
+    }
