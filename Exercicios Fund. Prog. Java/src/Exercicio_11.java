cb5f868d1d4bd808e5fb71d5141fdc2229111cb0
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 20:59:14 -0300

    Resolução exercício 11

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_11.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_11 {
+    public static void main(String[] args) {
+        //11. Preencher uma lista com 10 números
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+        lista.add(40);
+        lista.add(50);
+        lista.add(60);
+        lista.add(70);
+        lista.add(80);
+        lista.add(90);
+        lista.add(100);
+
+        System.out.println(lista);
+    }
+}
