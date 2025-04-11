63c35839c3f113d392dfe2744325aa4b1b50dc40
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 19:31:26 -0300

    Resolução exercício 01

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_01.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_01 {
+    public static void main(String[] args) {
+        //01. Contagem de 1 a 10
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++)
+            numeros.add(i);
+            System.out.println(numeros);
+        }
+    }
