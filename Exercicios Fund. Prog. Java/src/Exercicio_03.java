e6588b9d6d6028292223762d13a2f0fe27c5262d
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 19:43:05 -0300

    Resolução exercício 03

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_03 {
+    public static void main(String[] args) {
+        //03. Tabuada de um número
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
