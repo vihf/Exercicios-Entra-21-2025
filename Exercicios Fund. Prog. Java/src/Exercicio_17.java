8183eaaad7fa5cc32f759d435b1bc27ec4c78acd
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-11 21:22:52 -0300

    Resolução exercício 17

---
+++ Exercicios Fund. Prog. Java/src/Exercicio_17.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio_17 {
+    public static void main(String[] args) {
+        //17. Inverter uma lista
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(6);
+
+        Collections.reverse(numeros);
+        System.out.println(numeros);
+    }
+}
